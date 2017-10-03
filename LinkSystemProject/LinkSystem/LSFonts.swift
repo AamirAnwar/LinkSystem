194a8ace5ab43bd00851ae4f212cc3357f1b1e7e
Aamir Anwar <aamir.anwar021193@gmail.com>
2017-10-03 22:14:24 +0530

    Added Pageheading and Sectionheading fonts to LSFonts

--- LinkSystemProject/LinkSystem/LSFonts.swift
+++ LinkSystemProject/LinkSystem/LSFonts.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import UIKit
+/*
+ - 0 : "AvenirNext-Medium"
+ - 1 : "AvenirNext-DemiBoldItalic"
+ - 2 : "AvenirNext-DemiBold"
+ - 3 : "AvenirNext-HeavyItalic"
+ - 4 : "AvenirNext-Regular"
+ - 5 : "AvenirNext-Italic"
+ - 6 : "AvenirNext-MediumItalic"
+ - 7 : "AvenirNext-UltraLightItalic"
+ - 8 : "AvenirNext-BoldItalic"
+ - 9 : "AvenirNext-Heavy"
+ - 10 : "AvenirNext-Bold"
+ - 11 : "AvenirNext-UltraLight"
+ */
+let LSFontPageHeading = UIFont(name: "AvenirNext-Bold", size: 53)
+let LSFontSectionHeading = UIFont(name: "AvenirNext-DemiBold", size: 39)
